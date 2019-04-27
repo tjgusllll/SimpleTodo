3d8521b0324b9325cc776ad00243bf2fc1d87e46
조서현 <gavriela1996@gmail.com>
2019-04-27 16:55:12 +0900

    Initial Commit

---
+++ SimpleTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTodo
+//
+//  Created by 조서현 on 2019. 4. 27..
+//  Copyright © 2019년 조서현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
